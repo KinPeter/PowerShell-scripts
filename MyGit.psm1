82a3703b14958b380eeab99dd5381ae6de01ac7b
Peter Kin <kinpeter85@gmail.com>
2020-02-12 22:21:53 +0100

    Add branch cleanup script

--- MyGit.psm1
+++ MyGit.psm1
@@ -1,6 +1,6 @@
 function Get-BranchByIssue {
-  [cmdletbinding()]
-  [alias("cobr")]
+  [CmdletBinding()]
+  [Alias("mygit-co")]
   param(
     [Parameter(Mandatory=$true)][string]$issueNumber
   )
@@ -25,4 +25,76 @@ function Get-BranchByIssue {
 
   git checkout $path
   git pull origin $path
+}
+
+function Clear-Branches {
+  [CmdletBinding(DefaultParameterSetName = "Normal")]
+  [Alias("mygit-clup")]
+  param(
+    [parameter(ParameterSetName = "Normal")]
+    [Alias("n")]
+    [switch]
+    $normal,
+    
+    [parameter(ParameterSetName = "Soft")]
+    [Alias("s")]
+    [switch]
+    $soft,
+
+    [parameter(ParameterSetName = "Forced")]
+    [Alias("f")]
+    [switch]
+    $forced
+  )
+
+  if ( -Not $normal -and -Not $soft -and -Not $forced ) {
+    $normal = $true
+  }
+
+  $gitBranch = git branch
+  [string[]]$branches = @()
+  foreach($line in $gitBranch) {
+    if ($line.ToString().StartsWith("*") -or ($line.ToString().trim() -eq "master") -or ($line.ToString().trim() -eq "develop")) { continue }
+    $branches += $line.ToString().trim()
+  }
+
+  if ($soft) {
+    Write-Host "[+] Starting in SOFT mode..."
+    foreach ($branch in $branches) {
+      $response = git branch -d $branch 2>&1
+      if ($response -Match "not fully merged") {
+        Write-Host "[Kept branch] $branch"
+      } else {
+        Write-Host "[Deleted branch] $branch"
+      }
+    }  
+  }
+
+  if ($forced) {
+    Write-Host "[+] Starting in FORCED mode..."
+    foreach ($branch in $branches) {
+      $response = git branch -D $branch 
+      Write-Host "[Deleted branch] $branch"      
+    }  
+  }
+
+  if ($normal) {
+    Write-Host "[+] Starting in NORMAL mode..."
+    foreach ($branch in $branches) {
+      $response = git branch -d $branch 2>&1
+      if ($response -Match "not fully merged") {
+        Write-Host "[-] Could not delete $branch"
+        $answer = Read-Host "[?] Do you want to force delete it? (Y/n)"
+        if ($answer -eq "N" -or $answer -eq "n") {
+          Write-Host "[Kept branch] $branch"
+        } else {
+          $response = git branch -D $branch 
+          Write-Host "[Deleted branch] $branch"
+        }
+      }
+      else {
+        Write-Host "[Deleted branch] $branch"
+      }
+    }  
+  }
 }
