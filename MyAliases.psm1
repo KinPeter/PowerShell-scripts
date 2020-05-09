fba5bd4cf5a66946f2ed19aa5ee36503e2c88f11
Peter Kin <kinpeter85@gmail.com>
2020-05-09 12:40:08 +0200

    Edit aliases

--- MyAliases.psm1
+++ MyAliases.psm1
@@ -1,8 +1,8 @@
-function Open-PowershellProfile { code C:\Windows\System32\WindowsPowerShell\v1.0\Profile.ps1 }
+function Open-PowershellProfile { code C:\Users\kinpe\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 }
 Set-Alias -Name open-profile -Value Open-PowershellProfile
 
-function Use-NodeVersion10 { nvm use 10.19.0 }
+function Use-NodeVersion10 { nvm use 10.20.1 }
 Set-Alias -Name "nvm-10" -Value Use-NodeVersion10
 
-function Use-NodeVersion12 { nvm use 12.16.1 }
+function Use-NodeVersion12 { nvm use 12.16.3 }
 Set-Alias -Name "nvm-12" -Value Use-NodeVersion12
